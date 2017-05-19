187f9b111f50743e415ec76e2112efb8307ab62e
f41_ardu <f41thr@arcor.de>
2017-05-19 20:58:56 +0200

    Create detectArduino.py
    Check if Arduino is connected either to one of the usb devices /dev/usbx=0,..3 or /dev/ACMx=0...3. 
    Uno si mostly connected to ACMx. Duemilanove to USBx

---
+++ detectArduino.py
@@ -0,0 +1,27 @@
+#!/usr/bin/pyhton
+# detectArduino.py
+# thr / 2017-05-18
+
+import serial
+for com in range(0,4):
+  try:
+    PORT = '/dev/ttyACM'+str(com)
+    BAUD =  38400
+    board = serial.Serial(PORT,BAUD)
+    print 'Hardware: %s' % board.__str__()
+    board.close()
+    break
+  except:
+    print 'Kein Arduino an /dev/ttyACM'+str(com)
+
+for com in range(0,4):
+  try:
+    PORT = '/dev/ttyUSB'+str(com)
+    BAUD =  38400
+    board = serial.Serial(PORT,BAUD)
+    print 'Arduino erkannt an /dev/ttyUSB'+str(com)
+    print 'Hardware: %s' % board.__str__()
+    board.close()
+    break
+  except:
+   print 'Kein Arduino an /dev/ttyUSB'+str(com)
